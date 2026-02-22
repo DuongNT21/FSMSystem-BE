cfeb4886e541c2e5a8e2863307de956b7897bdbc
TRUNGPC\ADMIN <trungngoquang37@gmail.com>
2026-02-22 11:26:46 +0700

    Commit cloudinary implement for image upload

--- src/main/java/com/swp391_be/SWP391_be/entity/BaseEntity.java
+++ src/main/java/com/swp391_be/SWP391_be/entity/BaseEntity.java
@@ -9,17 +9,27 @@ import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @NotNull
     private int id;
+
+     @CreatedDate
+    @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
+
+    @LastModifiedDate
     private LocalDateTime updatedAt;
     private LocalDateTime deletedAt;
 }
--- src/main/java/com/swp391_be/SWP391_be/service/IBouquetService.java
+++ src/main/java/com/swp391_be/SWP391_be/service/IBouquetService.java
@@ -1,8 +1,10 @@
 package com.swp391_be.SWP391_be.service;
 
 
+import java.util.List;
+
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.swp391_be.SWP391_be.dto.request.bouquet.CreateBouquetRequest;
 import com.swp391_be.SWP391_be.dto.request.bouquet.GetBouquetCriteriaRequest;
@@ -11,9 +13,8 @@ import com.swp391_be.SWP391_be.dto.response.bouquet.BouquetListResponse;
 import com.swp391_be.SWP391_be.dto.response.pageResponse.PageResponse;
 import com.swp391_be.SWP391_be.entity.Bouquet;
 
-@Service
 public interface IBouquetService {
-  Bouquet createBouquet(CreateBouquetRequest bouquetRequest);
+  Bouquet createBouquet(CreateBouquetRequest bouquetRequest, List<MultipartFile> images);
   Bouquet getById(int id);
   Bouquet updateBouquet(UpdateBouquetRequest bouquet);
   void deleteBouquet(int id);
